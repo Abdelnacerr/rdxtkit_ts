3198e3a4e16b718a45bde35d308823f69efa0639
Abdelnacerr <abdelnacerr@gmail.com>
2021-07-25 11:46:20 +1000

    actions disptached in app.tsx

--- src/Redux/action-creators/index.ts
+++ src/Redux/action-creators/index.ts
@@ -1,41 +1,53 @@
 import { ActionType } from "../action-types-constants";
 import { v1 as uuid } from "uuid";
 
-export const createTodoActionCreator = ({ desc }: { desc: string; }) => {
-    return {
-    type: ActionType.CREATE_TODO,
-        payload: {
-            id: uuid(),
-            desc,
-            isComplete: false
-        }
-    };
+export const createTodoActionCreator = ({ desc }: { desc: string }) => {
+	return {
+		type: ActionType.CREATE_TODO,
+		payload: {
+			id: uuid(),
+			desc,
+			isComplete: false,
+		},
+	};
 };
 
-export const EditTodoActionCreator = ({ id, desc }: { id: string, desc: string; }) => {
-    return {
-    type: ActionType.CREATE_TODO,
-    payload: { id, desc }
-    };
+export const EditTodoActionCreator = ({
+	id,
+	desc,
+}: {
+	id: string;
+	desc: string;
+}) => {
+	return {
+		type: ActionType.EDIT_TODO,
+		payload: { id, desc },
+	};
 };
 
-export const toggleTodoActionCreator = ({ id, isComplete}: { id: string; isComplete: boolean;}) => {
-    return {
-    type: ActionType.TOGGLE_TODO,
-    payload: { id, isComplete }
-    };
+export const toggleTodoActionCreator = ({
+	id,
+	isComplete,
+}: {
+	id: string;
+	isComplete: boolean;
+}) => {
+	return {
+		type: ActionType.TOGGLE_TODO,
+		payload: { id, isComplete },
+	};
 };
 
 export const deleteTodoActionCreator = ({ id }: { id: string }) => {
-    return {
-    type: ActionType.DELETE_TODO,
-    payload: { id }
-    };
+	return {
+		type: ActionType.DELETE_TODO,
+		payload: { id },
+	};
 };
 
 export const selectTodoActionCreator = ({ id }: { id: string }) => {
-    return {
-    type: ActionType.DELETE_TODO,
-    payload: { id }
-    };
+	return {
+		type: ActionType.SELECT_TODO,
+		payload: { id },
+	};
 };
